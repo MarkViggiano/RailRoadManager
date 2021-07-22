dbe96793984955ae76cfda50e9f31d4847a41839
Flaymed <markcviggiano@gmail.com>
2021-07-22 12:15:17 -0400

    Base for Trains

--- utils/layout/Train.js
+++ utils/layout/Train.js
@@ -1,5 +1,38 @@
 class Train {
-  constructor() {
+  #name;
+  #cars;
+  #engines;
+
+  constructor(name) {
+    this.#name = name;
+    this.#cars = [];
+    this.#engines = [];
+  }
+
+  getName() {
+    this.#name;
+  }
+
+  getCars() {
+    return this.#cars;
+  }
+
+  getEngines() {
+    return this.#engines;
+  }
+
+  getTrainLength() {
+    let trainLength = 0;
+
+    for (const car of this.getCars()) {
+      trainLength += car.getLength();
+    }
+
+    for (const engine of this.getEngines()) {
+      trainLength += engine.getLength();
+    }
+
+    return trainLength;
 
   }
 }
